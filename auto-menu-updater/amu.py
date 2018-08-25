ae5bb9118f833e11c7e10b2b8d265ffc98e62b01
creewick <ilia_Y@mail.ru>
2018-08-25 14:46:18 +0500

    ready for review

--- auto-menu-updater/amu.py
+++ auto-menu-updater/amu.py
@@ -1,17 +1,70 @@
 from os import walk, rename, remove, sep
+from io import TextIOWrapper
 from os.path import join
 from sys import argv
 from re import match
 
 
+def main():
+    for folder, file in get_files(dir=argv[1]):
+        if is_md(file):
+            menu = get_menu(folder, root=argv[2])
+            try_replace_menu(folder, file, menu)
+
+
 def get_files(dir):
-    for path, _, files in walk(dir):
+    for folder, _, files in walk(dir):
         for name in files:
-            yield path, name
+            yield folder, name
 
 
-def is_md(filename):
-    return filename[-3:] == '.md'
+def is_md(file):
+    return file[-3:] == '.md'
+
+
+def get_menu(path, root):
+    folders = path.split(sep)
+    folders[0] = root
+    count = len(folders)
+
+    for i in range(count-1):
+        title = folders[i]
+        url = '../' * (count-1-i)
+
+        folders[i] = f'[{title}]({url})'
+
+    return ' > '.join(folders) + '\n\n'
+
+
+def try_replace_menu(folder, file, menu):
+    _path = join(folder, f'_{file}')
+    path = join(folder, file)
+    try:
+        rename(path, _path)
+        try:
+            with open(_path, 'r') as old:
+                with open(path, 'w') as new:
+                    replace_menu(old, new, menu)
+            remove(_path)
+        except:
+            rename(_path, path)
+    except:
+        pass
+
+
+def replace_menu(old: TextIOWrapper, new: TextIOWrapper, menu):
+    new.write(menu)
+    text_started = False
+    line = old.readline()
+
+    while line != '':
+        if not text_started and is_menu(line):
+            old.readline()
+        else:
+            text_started = True
+            new.write(line)
+
+        line = old.readline()
 
 
 def is_menu(line):
@@ -19,47 +72,4 @@ def is_menu(line):
                  line) is not None
 
 
-def get_menu(path, menu_name):
-    result = ''
-    folders = path.split(sep)
-
-    for i in range(len(folders) - 2):
-        title = folders[i]
-        url = '../' * (len(folders) - 2 - i)
-
-        result += f'[{title}]({url}) > '
-
-    return result + folders[-2] + '\n'
-
-
-def copy_with_menu(old, new, menu):
-    new.write(menu)
-    first = True
-
-    for line in old:
-        if not first or not is_menu(line):
-            new.write(line)
-            first = False
-
-
-def update_menu(path, name, menu_name):
-    _path = join(path, f'_{name}')
-    path = join(path, name)
-    try:
-        rename(path, _path)
-        try:
-            with open(_path, 'r') as old:
-                with open(path, 'w') as new:
-                    menu = get_menu(path, menu_name)
-                    copy_with_menu(old, new, menu)
-            remove(_path)
-        except:
-            rename(_path, path)
-    except:
-        return
-
-
-if __name__ == '__main__':
-    for path, name in get_files(dir=argv[1]):
-        if is_md(name):
-            update_menu(path, name, menu_name=argv[2])
+if __name__ == '__main__': main()
