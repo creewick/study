e828aa093dd6c5443ac765a4c43d0c0b6b664d6e
creewick <ilia_Y@mail.ru>
2018-08-29 00:41:34 +0500

    test;

--- auto-menu-updater/amu.py
+++ auto-menu-updater/amu.py
@@ -8,7 +8,7 @@ import logging
 
 def main():
     global latex_script
-    latex_script = """<script type="text/x-mathjax-config">MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});</script><script src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML' async></script>\n"""
+    latex_script = """<script type="text/x-mathjax-config">MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$']]}});</script><script src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML' async></script>\n"""
     logging.basicConfig(filename='log')
     args = get_args()
     menu_regex = compile(r'(\[.*?\]\((..\/)*\) > |{args.name})')
--- logic/0/lang.md
+++ logic/0/lang.md
@@ -1,4 +1,4 @@
-<script type="text/x-mathjax-config">MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\(','\)']]}});</script><script src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML' async></script>
+<script type="text/x-mathjax-config">MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$']]}});</script><script src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML' async></script>
 [home](../../) > [logic](../) > [0](./) > lang
 
 # Язык логики высказываний
