8d1937c4ebf7b0f9a2a585feb2ae23e59a99e8a3
creewick <ilia_Y@mail.ru>
2018-09-13 14:15:09 +0500

    'element' is not defined - fix

--- assets/header.js
+++ assets/header.js
@@ -2,5 +2,5 @@ var links = document.getElementsByClassName("page-link");
 
 for (var i = 0; i < links.length; i++) {
     if (document.URL.indexOf(links[i].href) > -1)
-        element.className += ' selected';
+        links[i].className += ' selected';
 };
