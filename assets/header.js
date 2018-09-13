4a4dbed8386e19346a8389c3c41b96c6cdf28e9e
creewick <ilia_Y@mail.ru>
2018-09-13 17:04:43 +0500

    css fixes

--- assets/header.js
+++ assets/header.js
@@ -2,6 +2,7 @@ var links = document.getElementsByClassName("page-link");
 
 for (var i = 0; i < links.length; i++) {
     if (links[i].href != "https://creewick.github.io/study/courses/" 
+        && links[i].href != ""
         && document.URL.indexOf(links[i].href) > -1)
         links[i].className += ' selected';
     if (links[i].href == "https://creewick.github.io/study/courses/" 
