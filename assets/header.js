8f795731be9d8fc2c71edbdf1ad006dc684eea3a
creewick <ilia_Y@mail.ru>
2018-09-13 14:23:19 +0500

    Все курсы не подчеркивается после выбора курса (fix)

--- assets/header.js
+++ assets/header.js
@@ -1,8 +1,8 @@
 var links = document.getElementsByClassName("page-link");
 
 for (var i = 0; i < links.length; i++) {
-    if (links[i] != 'study/courses/' && document.URL.indexOf(links[i].href) > -1)
+    if (links[i].href != 'study/courses/' && document.URL.indexOf(links[i].href) > -1)
         links[i].className += ' selected';
-    if (links[i] == 'study/courses/' && document.URL.endsWith(links[i].href))
+    if (links[i].href == 'study/courses/' && document.URL.endsWith(links[i].href))
         links[i].className += ' selected';
 };
