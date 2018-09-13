5f7436c7ab15d1f174d8d95431b7a5785c07f43f
creewick <ilia_Y@mail.ru>
2018-09-13 14:21:45 +0500

    Все курсы не подчеркивается после выбора курса

--- assets/header.js
+++ assets/header.js
@@ -1,6 +1,8 @@
 var links = document.getElementsByClassName("page-link");
 
 for (var i = 0; i < links.length; i++) {
-    if (document.URL.indexOf(links[i].href) > -1)
+    if (links[i] != 'study/courses/' && document.URL.indexOf(links[i].href) > -1)
+        links[i].className += ' selected';
+    if (links[i] == 'study/courses/' && document.URL.endsWith(links[i].href))
         links[i].className += ' selected';
 };
