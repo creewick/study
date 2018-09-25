0d9ab390b861a023fd82b28cca0955f6a3bbc10f
creewick <ilia_Y@mail.ru>
2018-09-25 16:10:11 +0500

    90%

--- assets/scroll-hide.js
+++ assets/scroll-hide.js
@@ -3,7 +3,7 @@ window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementsByClassName("scroll-hide")[0].style.top = "62px";
-    document.getElementsByClassName("site-header")[0].style.backgroundColor = "rgba(255, 255, 255, 0.8)";
+    document.getElementsByClassName("site-header")[0].style.backgroundColor = "rgba(255, 255, 255, 0.9)";
 
   } else {
     document.getElementsByClassName("scroll-hide")[0].style.top = "8px";
