a3dae83063f7aa3592346d7de3a47248d3874d66
creewick <ilia_Y@mail.ru>
2018-09-26 11:29:10 +0500

    css

--- assets/scroll-hide.js
+++ assets/scroll-hide.js
@@ -2,12 +2,26 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
+    
     document.getElementsByClassName("scroll-hide")[0].style.top = "62px";
-    document.getElementsByClassName("scroll-hide")[0].style.color = "white";
-
+    
+    var links = document
+      .getElementsByClassName("scroll-hide")[0]
+      .getElementsByClassName("page-link");
+    
+      for (let i = 0; i < links.length; i++) {
+        links[i].style.color = "black";
+      }
   } else {
     document.getElementsByClassName("scroll-hide")[0].style.top = "8px";
-    document.getElementsByClassName("scroll-hide")[0].style.color = "black";
+   
+    var links = document
+      .getElementsByClassName("scroll-hide")[0]
+      .getElementsByClassName("page-link");
+  
+    for (let i = 0; i < links.length; i++) {
+      links[i].style.color = "white";
+    }
   }
   prevScrollpos = currentScrollPos;
 } 
