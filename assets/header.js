36932dbfeb8b4417b950e7727b78b0b974d2ed11
creewick <ilia_Y@mail.ru>
2018-09-26 11:33:34 +0500

    css

--- assets/header.js
+++ assets/header.js
@@ -3,12 +3,18 @@ var links = document.getElementsByClassName("page-link");
 for (var i = 0; i < links.length; i++) {
     if (links[i].href != "https://creewick.github.io/study/courses/" 
         && links[i].href != ""
-        && document.URL.indexOf(links[i].href) > -1)
-        links[i].className += ' selected';
+        && document.URL.indexOf(links[i].href) > -1){
+            links[i].className += ' selected';
+            links[i].focus();
+        }
     if (links[i].href == "https://creewick.github.io/study/courses/" 
-        && document.URL.endsWith(links[i].href))
-        links[i].className += ' selected';
+        && document.URL.endsWith(links[i].href)){
+            links[i].className += ' selected';
+            links[i].focus();
+        }
 };
 
-if (document.URL == 'https://creewick.github.io/study/')
+if (document.URL == 'https://creewick.github.io/study/'){
     document.getElementsByClassName('site-title')[0].className += ' selected';
+    document.getElementsByClassName('site-title')[0].focus();
+}
--- assets/scroll-hide.js
+++ assets/scroll-hide.js
@@ -2,26 +2,9 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    
     document.getElementsByClassName("scroll-hide")[0].style.top = "62px";
-    
-    var links = document
-      .getElementsByClassName("scroll-hide")[0]
-      .getElementsByClassName("page-link");
-    
-      for (let i = 0; i < links.length; i++) {
-        links[i].style.color = "black";
-      }
   } else {
     document.getElementsByClassName("scroll-hide")[0].style.top = "8px";
-   
-    var links = document
-      .getElementsByClassName("scroll-hide")[0]
-      .getElementsByClassName("page-link");
-  
-    for (let i = 0; i < links.length; i++) {
-      links[i].style.color = "white";
-    }
   }
   prevScrollpos = currentScrollPos;
 } 
