93f273c9e9d15659f11be2b4ad5c814a95269790
creewick <ilia_Y@mail.ru>
2018-09-26 17:29:34 +0500

    shadow

--- assets/scroll-hide.js
+++ assets/scroll-hide.js
@@ -10,7 +10,7 @@ window.onscroll = function() {
   }
 
   if (pageYOffset > 0) {
-    menu.style.boxShadow = "0 15px 20px -20px";
+    menu.style.boxShadow = "0 10px 10px -15px";
   } else {
     menu.style.boxShadow = "none";
   }
