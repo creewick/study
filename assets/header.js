e9612ea55e060338615deed886d658691477f9b1
creewick <ilia_Y@mail.ru>
2018-09-26 15:06:33 +0500

    refactor js

--- assets/header.js
+++ assets/header.js
@@ -1,20 +1,22 @@
 var links = document.getElementsByClassName("page-link");
 
 for (var i = 0; i < links.length; i++) {
-    if (links[i].href != "https://creewick.github.io/study/courses/" 
-        && links[i].href != ""
-        && document.URL.indexOf(links[i].href) > -1){
-            links[i].className += ' selected';
-            links[i].focus();
+    if (links[i].href != "https://creewick.github.io/study/courses/") {
+        if (document.URL.indexOf(links[i].href) > -1) {
+            select(links[i]);
         }
-    if (links[i].href == "https://creewick.github.io/study/courses/" 
-        && document.URL.endsWith(links[i].href)){
-            links[i].className += ' selected';
-            links[i].focus();
+    } else {
+        if (document.URL.endsWith(links[i].href)) {
+            select(links[i]);
         }
+    }
 };
 
 if (document.URL == 'https://creewick.github.io/study/'){
-    document.getElementsByClassName('site-title')[0].className += ' selected';
-    document.getElementsByClassName('site-title')[0].focus();
+    select(document.getElementsByClassName('site-title')[0]);
+}
+
+function select(link) {
+    link.className += ' selected';
+    link.focus();
 }
