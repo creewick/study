de48098352322aa8cc3b652a3f7c737fbe1a4a96
creewick <ilia_Y@mail.ru>
2018-09-26 15:53:12 +0500

    test

--- assets/scroll-hide.js
+++ assets/scroll-hide.js
@@ -2,9 +2,9 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementsByClassName("scroll-hide")[0].className = "scroll-hide open";
+    document.getElementsByClassName("scroll-hide")[0].className = "menu wrapper scroll-hide open";
   } else {
-    document.getElementsByClassName("scroll-hide")[0].className = "scroll-hide";
+    document.getElementsByClassName("scroll-hide")[0].className = "menu wrapper scroll-hide";
   }
   prevScrollpos = currentScrollPos;
 } 
